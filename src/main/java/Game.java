255977306a4d67798ee9d8beb65ccc4350f28ae7
Narit Suwannarat <narit@MacBook-Pro-khxng-Narit.local>
2017-10-24 22:33:44 +0700

    test commit2

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,12 +3,10 @@ import javax.swing.*;
 public class Game extends JFrame {
 
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame("NOOB FREN");
+        JFrame jFrame = new JFrame("Two Pick");
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setContentPane(new GamePanel());
         jFrame.pack();
         jFrame.setVisible(true);
     }
 }
-
-// Noob FREN
